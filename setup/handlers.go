cde8e655038c66a95588164641c705513be03cd4
junior <junior@edenfarm.id>
2024-06-26 23:24:27 +0700

    chore: tidying up folders

--- setup/handlers.go
+++ setup/handlers.go
@@ -3,19 +3,13 @@ package setup
 import (
 	albumHandler "harmoniq/harmoniq-api-v2/service/album/delivery/http"
 	artistHandler "harmoniq/harmoniq-api-v2/service/artist/delivery/http"
-	cartHandler "harmoniq/harmoniq-api-v2/service/cart/delivery/http"
-	categoryHandler "harmoniq/harmoniq-api-v2/service/category/delivery/http"
-	productHandler "harmoniq/harmoniq-api-v2/service/product/delivery/http"
 	userHandler "harmoniq/harmoniq-api-v2/service/user/delivery/http"
 
 	"github.com/labstack/echo"
 )
 
 func SetupHandlers(e *echo.Echo, useCases *UseCases) {
-	productHandler.NewProductHandler(e, useCases.ProductUsecase)
 	userHandler.NewUserHandler(e, useCases.UserUsecase)
-	categoryHandler.NewCategoryHandler(e, useCases.CategoryUsecase)
-	cartHandler.NewCartHandler(e, useCases.CartUsecase)
 	albumHandler.NewAlbumHandler(e, useCases.AlbumUsecase)
 	artistHandler.NewArtistHandler(e, useCases.ArtistUsecase)
 }
--- setup/usecases.go
+++ setup/usecases.go
@@ -3,9 +3,6 @@ package setup
 import (
 	albumUseCase "harmoniq/harmoniq-api-v2/service/album/usecase"
 	artistUsecase "harmoniq/harmoniq-api-v2/service/artist/usecase"
-	cartUseCase "harmoniq/harmoniq-api-v2/service/cart/usecase"
-	categoryUsecase "harmoniq/harmoniq-api-v2/service/category/usecase"
-	productUseCase "harmoniq/harmoniq-api-v2/service/product/usecase"
 	userUseCase "harmoniq/harmoniq-api-v2/service/user/usecase"
 
 	"harmoniq/harmoniq-api-v2/domain"
@@ -14,21 +11,15 @@ import (
 )
 
 type UseCases struct {
-	ProductUsecase  domain.ProductUsecase
-	UserUsecase     domain.UserUsecase
-	CategoryUsecase domain.CategoryUsecase
-	CartUsecase     domain.CartUsecase
-	AlbumUsecase    domain.AlbumUsecase
-	ArtistUsecase   domain.ArtistUsecase
+	UserUsecase   domain.UserUsecase
+	AlbumUsecase  domain.AlbumUsecase
+	ArtistUsecase domain.ArtistUsecase
 }
 
 func NewUseCases(repos *Repositories, timeout time.Duration) *UseCases {
 	return &UseCases{
-		ProductUsecase:  productUseCase.NewProductUsecase(repos.ProductRepo, repos.CategoryRepo, repos.ProductImageRepo, timeout),
-		UserUsecase:     userUseCase.NewUserUsecase(repos.UserRepo, timeout),
-		CategoryUsecase: categoryUsecase.NewCategoryUsecase(repos.CategoryRepo, timeout),
-		CartUsecase:     cartUseCase.NewCartUsecase(repos.CartRepo, repos.ProductRepo, repos.CategoryRepo, repos.ProductImageRepo, timeout),
-		AlbumUsecase:    albumUseCase.NewAlbumUsecase(repos.AlbumRepo, repos.ArtistRepo, timeout),
-		ArtistUsecase:   artistUsecase.NewArtistUsecase(repos.ArtistRepo, timeout),
+		UserUsecase:   userUseCase.NewUserUsecase(repos.UserRepo, timeout),
+		AlbumUsecase:  albumUseCase.NewAlbumUsecase(repos.AlbumRepo, repos.ArtistRepo, timeout),
+		ArtistUsecase: artistUsecase.NewArtistUsecase(repos.ArtistRepo, timeout),
 	}
 }
